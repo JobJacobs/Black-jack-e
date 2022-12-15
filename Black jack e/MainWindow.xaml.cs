9bc6ebce0e610b5474a9e70a31902ac1d57416f3
JobJacobs <12002301@student.pxl.be>
2022-12-15 10:15:03 +0000

    Add project files.

---
+++ Black jack e/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Black_jack_e
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //random is type rnd is naam
+        Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDeel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //apend line 
+        //apend
+        private void random()
+        {
+            cardValue = rnd.next(1, 14);
+           
+        }
+        private
+    }
+    
+}
